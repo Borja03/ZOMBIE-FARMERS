43ab7b4772a5f16fbe53a4159e2081c9e3e1a62f
Borja <borjaahedo@gmail.com>
2025-01-13 16:59:41 +0100

    timer and coins

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -46,11 +46,10 @@ public class Enemy : MonoBehaviour
     {
         if (player == null) return; // Asegurarse de que el jugador esté asignado
 
-        // Calcular la dirección hacia la posición actual del jugador
-        Vector3 direction = (player.position - transform.position).normalized;
+        // Calcular la dirección hacia la posición actual del jugador en 2D
+        Vector2 direction = (player.position - transform.position).normalized;
 
-        // Mover al enemigo hacia la posición del jugador de manera suave
-        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        // Mover al enemigo hacia la posición del jugador en 2D
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     }
-
 }
