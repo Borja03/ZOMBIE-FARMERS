71c0264074d00dbfebe43ad65ae5683ccd97f117
Borja03 <162575763+Borja03@users.noreply.github.com>
2025-02-03 13:20:40 +0100

    push

--- Assets/Undead Survivor/Codes/PoolManager.cs
+++ Assets/Undead Survivor/Codes/PoolManager.cs
@@ -20,19 +20,16 @@ public class PoolManager : MonoBehaviour {
     public GameObject Get(int index) {
         GameObject select = null;
 
-        // ... 선택한 풀의 놀고 (비활성화 된) 있는 게임오브젝트 접근
+       
         foreach (GameObject item in pools[index]) {
             if (!item.activeSelf) {
-                // ... 발견하면 select 변수에 할당
                 select = item;
                 select.SetActive(true);
                 break;
             }
         }
 
-        // ... 못 찾았으면?
-        if(!select) {
-            // ... 새롭게 생성하고 select 변수에 할당
+        if(!select) {  
             select = Instantiate(prefabs[index], transform);
             pools[index].Add(select);
         }
